3cd49f580f33529ed5301018313e5db90a311bf3
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-03-28 18:38:11 +0530

    axios

--- api/src/App.jsx
+++ api/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import AxiosExample from "./axios";
 import FetchExample from "./fetch";
 
 function App() {
   return (
     <>
       <h1>Yash Bhawsar</h1>
-      <FetchExample />
+      {/* <FetchExample /> */}
+      <AxiosExample />
     </>
   );
 }
---
+++ api/src/axios.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const AxiosExample = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => setData(response.data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      {data.map((post) => (
+        <div key={post.id}>
+          <h4>{post.title}</h4>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AxiosExample;
